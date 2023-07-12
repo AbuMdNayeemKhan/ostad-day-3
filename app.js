3bedba869d37a686db9d1a1fc47e5efb51a370aa
unknown <abumohammadnayemkhan@gmail.com>
2023-07-12 23:42:58 +0600

    Learning JavaScript

---
+++ app.js
@@ -0,0 +1,37 @@
+//==========ES6 START==========//
+
+//===== spread operator start =====//
+// let numOne = [1, 2, 3];
+// let numTwo = [4, 5, 6];
+// var numbersCombined = [...numOne, ...numTwo];
+// console.log(numbersCombined);
+
+var personOne = {
+    name: "Nayeem Khan",
+    model: "Mustang",
+    color: "Red",
+}
+
+var personTwo = {
+    name: "Na Khan",
+    model: "Mustang",
+    color: "Green",
+}
+//===== spread operator ended =====//
+
+//===== Rest Parameter =====//
+function myFun(...data){
+    console.log(Args[2]);
+};
+//===== Rest Parameter =====//
+
+
+
+
+
+
+
+
+
+//==========ES6 ENDED==========//
+
